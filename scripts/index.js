68d5d339f224678ee7a2b5e8aed321e3c7741b9f
keanritcher <44213751+keanritcher@users.noreply.github.com>
2018-10-17 02:15:12 +0100

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -89,7 +89,7 @@ function checkMessage() {
 
 function ajaxify() {
   $.ajax({
-    url : "scripts/talktome.php",
+    url : "talktome.php",
     type: "POST",
     data : $("form").serialize(),
     success: function() {
