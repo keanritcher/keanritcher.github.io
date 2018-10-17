618620e90b505df4656fa74d2c60d136a6ab2597
keanritcher <44213751+keanritcher@users.noreply.github.com>
2018-10-17 02:02:16 +0100

    Update talktome.php

--- scripts/talktome.php
+++ scripts/talktome.php
@@ -1,6 +1,19 @@
-<Location "/">
-  AllowMethods GET POST OPTIONS
-</Location>
+// array holding allowed Origin domains
+$allowedOrigins = array(
+  'https://keanritcher.github.io'
+);
+ 
+if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] != '') {
+  foreach ($allowedOrigins as $allowedOrigin) {
+    if (preg_match('#' . $allowedOrigin . '#', $_SERVER['HTTP_ORIGIN'])) {
+      header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+      header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+      header('Access-Control-Max-Age: 1000');
+      header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+      break;
+    }
+  }
+}
 
 
 <?php
